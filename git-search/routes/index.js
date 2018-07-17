dafc198a8e2be5750f827db5cb3da1748991a55a
Andy Liang <andy.liang.1@stonybrook.edu>
2018-07-17 15:57:23 -0700

    API Data Retrieved and Modified

--- git-search/routes/index.js
+++ git-search/routes/index.js
@@ -19,15 +19,18 @@ router.post('/results', (req, res) => {
 
 // GET Results
 router.get('/results', (req, res) => {
-  const search = 'tetris';
-  const language = 'assembly';
+  const search = 'costco';
+  const language = 'ruby';
+  const link = 'https://api.github.com/search/repositories?q=' + search + '+language:' + language + "&sort=stars&order=desc";
   const options = {
-    url: 'https://api.github.com/search/repositories?q=' + search + '+language:' + language + "&sort=stars&order=desc",
+    url: link,
     method: 'GET',
     headers: {
       'User-Agent': 'aliang6',
     }
   };
+
+
 
   /* const option = {
     url: 'https://api.github.com/search/repositories?q=' + search + '+language:' + language + '&' + 'topic:open source',
@@ -40,9 +43,22 @@ router.get('/results', (req, res) => {
   request.get(options, (err, response, body) => {
     checkError(err);
     body = JSON.parse(body);
-    console.log(body);
+    for (let item of body.items) {
+      let tmp = item.created_at.substring(0, 10);
+      tmp += ' ' + item.created_at.substring(11, 19);
+      item.created_at = tmp;
+
+      tmp = item.updated_at.substring(0, 10);
+      tmp += ' ' + item.updated_at.substring(11, 19);
+      item.updated_at = tmp;
+      if(item.license === null){
+        item.license = { name: 'No License', };
+      }
+    }
+
+    //console.log(body);
     const total_count = body.total_count;
-    res.render('result', { body });
+    res.render('result', { body, link });
   });
 });
 
