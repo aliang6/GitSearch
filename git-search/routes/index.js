f68234d052339f240b7f674f9faffd1fead3c7b4
Andy Liang <andy.liang.1@stonybrook.edu>
2018-07-17 09:31:03 -0700

    Added API Url

--- git-search/routes/index.js
+++ git-search/routes/index.js
@@ -1,9 +1,35 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const request = require('request');
+
+const router = express.Router();
+
+function checkError(err) {
+  if (err) { console.log(err) };
+}
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res) => {
+  const search = 'tetris';
+  const langauge = 'assembly';
+  const url = 'https://api.github.com/search/repositories?q=' + 
+
+
+  request.get(, (err, response, body) => {
+    checkError(err);
+    body = JSON.parse(body);
+    console.log(body);
+  });
   res.render('index', { title: 'Express' });
+});
+
+// POST Results
+router.post('/results', (req, res) => {
+
+});
+
+// GET Results
+router.get('/results', (req, res) => {
+
 });
 
 module.exports = router;
