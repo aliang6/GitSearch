e0d616103bec154cb2aef87efb92e6927fa5fd7e
Andy Liang <andy.liang.1@stonybrook.edu>
2018-07-17 16:58:16 -0700

    Added Repository Display and Form Groups for Keywords and Languages

--- git-search/routes/index.js
+++ git-search/routes/index.js
@@ -7,6 +7,8 @@ function checkError(err) {
   if (err) { console.log(err) };
 }
 
+var input = "";
+
 /* GET home page. */
 router.get('/', (req, res) => {
   res.render('index', { title: 'Express' });
@@ -14,14 +16,15 @@ router.get('/', (req, res) => {
 
 // POST Results
 router.post('/results', (req, res) => {
-
+  console.log(req.body);
+  input = req.body.input;
+  res.redirect('/results');
 });
 
 // GET Results
 router.get('/results', (req, res) => {
-  const search = 'costco';
-  const language = 'ruby';
-  const link = 'https://api.github.com/search/repositories?q=' + search + '+language:' + language + "&sort=stars&order=desc";
+  const language = '';
+  const link = 'https://api.github.com/search/repositories?q=open source ' + input + '+language:' + language + "&sort=stars&order=desc&is:public";
   const options = {
     url: link,
     method: 'GET',
@@ -30,24 +33,15 @@ router.get('/results', (req, res) => {
     }
   };
 
-
-
-  /* const option = {
-    url: 'https://api.github.com/search/repositories?q=' + search + '+language:' + language + '&' + 'topic:open source',
-    method: 'GET',
-    headers: {
-      'User-Agent': 'aliang6',
-    }
-  } */
-
   request.get(options, (err, response, body) => {
     checkError(err);
     body = JSON.parse(body);
+    console.log(body);
     for (let item of body.items) {
+      // Format dates and time for created and updated
       let tmp = item.created_at.substring(0, 10);
       tmp += ' ' + item.created_at.substring(11, 19);
       item.created_at = tmp;
-
       tmp = item.updated_at.substring(0, 10);
       tmp += ' ' + item.updated_at.substring(11, 19);
       item.updated_at = tmp;
@@ -55,9 +49,8 @@ router.get('/results', (req, res) => {
         item.license = { name: 'No License', };
       }
     }
-
-    //console.log(body);
     const total_count = body.total_count;
+    console.log(total_count);
     res.render('result', { body, link });
   });
 });
